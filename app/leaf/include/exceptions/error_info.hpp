70a5cc0181179cd856743f8a6c90cf0a9081eded
Loïc Lopez <loic.lopez@epitech.eu>
2021-06-10 14:34:56 +0200

    fix "using" should be preferred to "typedef" for type aliasing.

--- app/leaf/include/exceptions/error_info.hpp
+++ app/leaf/include/exceptions/error_info.hpp
@@ -8,9 +8,8 @@
 #include <boost/exception/error_info.hpp>
 
 namespace Leaf::Exceptions::ErrorInfo {
-    typedef boost::error_info<struct errinfo_server_config_file_path_, std::string> errinfo_server_config_file_path;
-    typedef boost::error_info<struct errinfo_mime_types_config_file_path_, std::string> errinfo_mime_types_config_file_path;
-
+    using errinfo_server_config_file_path = boost::error_info<struct errinfo_server_config_file_path_, std::string>;
+    using errinfo_mime_types_config_file_path = boost::error_info<struct errinfo_mime_types_config_file_path_, std::string>;
 }
 
 #endif //LEAF_ERROR_INFO_HPP
